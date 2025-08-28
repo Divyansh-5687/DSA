3dc9c78a66c82b5cc05a2048eea7e10d8fab7e1b
Divyansh-5687 <divyanshsingh5687@gmail.com>
2025-08-28 18:15:03 +0530

    initial Commit.

---
+++ DSA/src/com/nt/arrayString/EncodeDecode.java
@@ -0,0 +1,45 @@
+package com.nt.arrayString;
+
+import java.util.*;
+
+ class Codec{
+	
+	public String encode (List<String>strs) {
+		if(strs.size()==0) {
+			return Character.toString((char)258);
+		}
+		String separate = Character.toString((char)257);
+		StringBuilder sb = new StringBuilder();
+		for(String s : strs) {
+			sb.append(s);
+			sb.append(separate);
+		}
+		sb.deleteCharAt(sb.length()-1);
+		return sb.toString();
+	}
+	
+	public List<String> decode (String s){
+		if(s.equals(Character.toString((char)258))) {
+			return new ArrayList();
+		}
+		String separate = Character.toString((char)257);
+		return Arrays.asList(s.split(separate,-1));
+	}
+}
+
+public class EncodeDecode {
+	public static void main(String[] args) {
+        Codec c = new Codec();
+
+        // Example input
+        List<String> strs = Arrays.asList("hello", "world", "java", "encode/decode");
+
+        // Encode
+        String encoded = c.encode(strs);
+        System.out.println("Encoded: " + encoded);
+
+        // Decode
+        List<String> decoded = c.decode(encoded);
+        System.out.println("Decoded: " + decoded);
+	}     
+}
